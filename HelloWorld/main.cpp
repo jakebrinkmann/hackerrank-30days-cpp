90e49cda9b54c610ae29d0dfafe3b0aa8ffe1710
Jake Brinkmann <jakebrinkmann@gmail.com>
2016-07-22 13:23:34 -0500

    Complete Day0: Hello, World

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string my_string;
+    cin>>my_string;
+    cout<<"\nHello World\n\n"<<endl;
+    cout<<my_string<<endl;
+    return(0);
+}
