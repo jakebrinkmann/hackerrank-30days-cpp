cdda97851218ef208a4b923ba9a41c39d1bb0531
Jake Brinkmann <jakebrinkmann@gmail.com>
2016-07-28 12:25:36 -0500

    Complete Day8: Dictionaries and Maps

---
+++ Maps/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main(){
+    int N;
+    string name;
+    int number;
+    string search;
+    cin >> N;
+    map<string,int> phoneBook;
+    for(int i=0; i<N; i++){
+        cin >> name;
+        cin >> number;
+        phoneBook[name] = number;
+    }
+    while (true) {
+        cin >> search;
+        if(phoneBook.find(search) != phoneBook.end()){
+            cout << search << "=" << phoneBook[search] <<endl;
+        } else {
+            cout << "Not found" <<endl;
+        }
+    }
+    return 0;
+}
