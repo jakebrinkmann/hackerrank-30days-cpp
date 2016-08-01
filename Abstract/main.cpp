ff0d62bd01e2dd594d25083457ddfa44ee4b6243
Jake Brinkmann <jakebrinkmann@gmail.com>
2016-08-01 11:38:40 -0500

    Construct the MyBook class

--- Abstract/main.cpp
+++ Abstract/main.cpp
@@ -17,7 +17,16 @@ class Book{
         virtual void display()=0;
 
 };
-//Write MyBook class
+
+class MyBook : public Book{
+    private:
+        int price;
+    public:
+        MyBook(string title, string author, int price) : Book(title, author)
+            { this->price = price; };
+        void display(void)
+            { cout << this->title << endl; };
+};
 
 int main() {
     string title,author;
