d9c3f14f384e360e48d224aa68cfbc4eaee8c671
Jake Brinkmann <jakebrinkmann@gmail.com>
2016-08-01 10:52:18 -0500

    Describe Day13 challenge

---
+++ Abstract/main.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Book{
+    protected:
+        string title;
+        string author;
+    public:
+        Book(string t,string a){
+            title=t;
+            author=a;
+        }
+        virtual void display()=0;
+
+};
+//Write MyBook class
+
+int main() {
+    string title,author;
+    int price;
+    getline(cin,title);
+    getline(cin,author);
+    cin>>price;
+    MyBook novel(title,author,price);
+    novel.display();
+    return 0;
+}
+
